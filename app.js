ea44cafaf37fdc0872014c083c16a1a6fc16ab92
고병옥 <7942apdkfl@hanmail.net>
2020-05-15 08:19:51 +0000

    테스트중

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ var http = require('http');
 var fs = require('fs');
 var index = fs.readFileSync('index.html');
 console.log('portttttttttttttttt ' +process.env.PORT);
+
+var port = process.env.PORT;
+
 http.createServer(function (req, res){
     res.writeHead(200, {'Context-Type': 'text/plain'});
     res.end(index);
-}).listen(8080);
+}).listen(port);
